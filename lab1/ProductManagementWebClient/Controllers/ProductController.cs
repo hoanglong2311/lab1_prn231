fd2e6d8fe638ad2369c27253a602b5398e7fcf31
Ha Thi Thuy Men <hamen.work@gmail.com>
2023-01-11 22:35:13 +0700

    Client app

---
+++ lab1/ProductManagementWebClient/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessObjects;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace ProductManagementWebClient.Controllers
+{
+    public class ProductController : Controller
+    {
+        public readonly HttpClient client = null;
+        public string ProductApiUrl = "";
+        public ProductController()
+        {
+            client = new HttpClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            client.DefaultRequestHeaders.Accept.Add(contentType);
+            ProductApiUrl = "http://localhost:53633/api/products";
+        }
+        public async Task<IActionResult> Index()
+        {
+            HttpResponseMessage response = await client.GetAsync(ProductApiUrl);
+            string strData = await response.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            List<Product> listProducts = JsonSerializer.Deserialize<List<Product>>(strData, options);
+            return View(listProducts);
+        }
+    }
+}
